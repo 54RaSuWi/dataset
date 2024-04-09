07d5180b28956798213a6c833c49db7e3fe01ef5
Rafly Surya Wibowo <47868660+54RaSuWi@users.noreply.github.com>
2024-04-10 02:39:03 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel
 import streamlit as st
 import pickle
 import pandas as pd
+import numpy as np
 import uvicorn
+from typing import List
 
 # Load ML model
 def open_model(model_path):
@@ -12,18 +14,18 @@ def open_model(model_path):
         model = pickle.load(model_file)
     return model
 
-model_last = open_model("https://github.com/54RaSuWi/dataset/blob/main/model.pkl") 
+model_last = open_model("D:/Kelas/Undip/SI/python/API/model.pkl") 
 
 # Create application
 app = FastAPI()
 
 class HeartDisease(BaseModel):
     generasi: str = Query(..., description="umur 12-27 (gen Z), umur 28-43 (millenials/gen Y), umur 44-59 (gen X), umur 60-78 (baby boomer)")
-    gender: str = Query(..., description="Jenis kelamin anda (0 = wanita, 1 = pria)")
+    gender: int = Query(..., description="Jenis kelamin anda (0 = wanita, 1 = pria)")
     cp: int = Query(..., description="Jenis penyakit dada yang diderita (0 = typical angina, Value 1 = atypical angina, Value 2 = non-anginal pain, Value 3 = asymptomatic)")
     trestbps: int = Query(..., description="Tekanan darah (0 = Normal, 1 = Hipertensi 1, 2 = Hipertensi 2, 3 = Hipertensi 3)")
     chol: int = Query(..., description="kadar kolesterol (0 = normal, 1 = berisiko, 2 = tinggi)")
-    fbs: float = Query(..., description="Kadar gula (0 = kurang dari sama 120 mg/dl, 1 = lebih dari 120 mg/dl)")
+    fbs: int = Query(..., description="Kadar gula (0 = kurang dari sama 120 mg/dl, 1 = lebih dari 120 mg/dl)")
     restecg: int = Query(..., description="Kondisi EKG (0 = normal, 1 = hipertrofi vertikel kiri, 2 = gelombang kelainan ST-T)")
     thalach: float = Query(..., description="Detak jantung maksimum")
     exang: int = Query(..., description="Apakah dada nyeri saat berolahraga? (0 = tidak nyeri, 1 = nyeri)")
@@ -31,10 +33,6 @@ class HeartDisease(BaseModel):
     slope: int = Query(..., description="Kemiringan depresi ST selepas berolahraga (0 = upsloping, 1 = flat, 2 = downsloping)")
     ca: int = Query(..., description="Banyaknya pembuluh darah yang terdeteksi oleh fluoroskopi (0,1,2,3,4)")
     thal: int = Query(..., description="Jenis thalasemia (0 = normal, 1 = fixed defect (permanen), 2 & 3 : reversal defect (sementara))")
-
-# Define FastAPI endpoint URL
-base_url = "http://localhost:8000"
-
 
 @app.post('/predict')
 def predict():
@@ -52,11 +50,10 @@ def predict():
     exang = st.selectbox("Nyeri dada saat olahraga", [0, 1])
     oldpeak = st.number_input("Perubahan depresi ST saat olahraga relatif saat istirahat", value=0.0)
     slope = st.selectbox("Kemiringan ST setelah olaraga", [0, 1, 2])
-    ca = st.selectbox("Jumlah pembuluh darah besar yang terdeteksi", [0, 1, 2, 4])
+    ca = st.selectbox("Jumlah pembuluh darah besar yang terdeteksi", [0, 1, 2, 3])
     thal = st.selectbox("Jenis thalasemia", [0, 1, 2, 3])
 
-
-    if st.button("Predict"):
+    if st.button("predict"):
         data = HeartDisease(
             generasi=generasi,
             gender=gender,
@@ -72,13 +69,15 @@ def predict():
             ca=ca,
             thal=thal
         )
-
-        prediction = model_last.predict(data)
-        output = prediction.json()
-        if output == 1:
-            result = 'Pasien mempunyai risiko terkena penyakit jantung'                    
+        print("Predicting.....")
+        prediction = model_last.predict(data.reshape(-1, 1))
+        if prediction == 1:
+            prediction = 'Pasien mempunyai risiko terkena penyakit jantung'                    
         else:
-            result = 'Pasien tidak mempunyai risiko terkena penyakit jantung'
-    
+            prediction = 'Pasien tidak mempunyai risiko terkena penyakit jantung'
+        return {
+        'prediction': prediction
+    }
+
 if __name__ == "__main__":
-    uvicorn.run(app)
+    predict()
